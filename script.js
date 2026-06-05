b5197f2b67e69ddfbdc570e6903180cb8e43e951
Michelle-Mutua <michelle-mutua@MacBookPro.lan>
2026-06-05 11:44:35 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Find the button using its HTML id
+const button = document.getElementById('order-btn');
+
+// 2. Make it do something when clicked
+button.addEventListener('click', function() {
+    alert("Order placed!");
+});
